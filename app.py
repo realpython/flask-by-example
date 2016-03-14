47c4c936f8079047a2195d0d8daeb5b0ba711a82
Michael Herman <hermanmu@gmail.com>
2016-03-14 08:36:03 -0600

    updated app.py

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+
+@app.route('/<name>')
+def hello_name(name):
+    return "Hello {}!".format(name)
+
 if __name__ == '__main__':
     app.run()
