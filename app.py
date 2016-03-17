f47b1ce1b64ffe6a9c34b714472945cd66320a52
Michael Herman <hermanmu@gmail.com>
2016-03-17 16:39:59 -0600

    part 4

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import requests
 import operator
 import re
 import nltk
-from flask import Flask, render_template, request
+from rq import Queue
+from rq.job import Job
+from worker import conn
+from flask import Flask, render_template, request, jsonify
 from flask.ext.sqlalchemy import SQLAlchemy
 from stop_words import stops
 from collections import Counter
@@ -15,53 +18,84 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-from models import Result
+q = Queue(connection=conn)
+
+from models import *
+
+
+def count_and_save_words(url):
+
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text).get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
     results = {}
     if request.method == "POST":
         # get url that the person has entered
-        try:
-            url = request.form['url']
-            r = requests.get(url)
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
-            )
-            return render_template('index.html', errors=errors)
-        if r:
-            # text processing
-            raw = BeautifulSoup(r.text).get_text()
-            nltk.data.path.append('./nltk_data/')  # set the path
-            tokens = nltk.word_tokenize(raw)
-            text = nltk.Text(tokens)
-            # remove punctuation, count raw words
-            nonPunct = re.compile('.*[A-Za-z].*')
-            raw_words = [w for w in text if nonPunct.match(w)]
-            raw_word_count = Counter(raw_words)
-            # stop words
-            no_stop_words = [w for w in raw_words if w.lower() not in stops]
-            no_stop_words_count = Counter(no_stop_words)
-            # save the results
-            results = sorted(
-                no_stop_words_count.items(),
-                key=operator.itemgetter(1),
-                reverse=True
-            )[:10]
-            try:
-                result = Result(
-                    url=url,
-                    result_all=raw_word_count,
-                    result_no_stop_words=no_stop_words_count
-                )
-                db.session.add(result)
-                db.session.commit()
-            except:
-                errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
+        url = request.form['url']
+        if 'http://' not in url[:7]:
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+
+    return render_template('index.html', results=results)
+
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
+    else:
+        return "Nay!", 202
 
 
 if __name__ == '__main__':
